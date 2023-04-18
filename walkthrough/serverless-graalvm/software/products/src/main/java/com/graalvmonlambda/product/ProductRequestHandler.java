c238b8f79e31e5ef7f5ad7621ee955a2ed7beaed
sgao <sgao@beckman.com>
2023-04-18 11:31:44 +0800

    update

--- walkthrough/serverless-graalvm/software/products/src/main/java/com/graalvmonlambda/product/ProductRequestHandler.java
+++ walkthrough/serverless-graalvm/software/products/src/main/java/com/graalvmonlambda/product/ProductRequestHandler.java
@@ -3,11 +3,11 @@ package com.graalvmonlambda.product;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class ProductRequestHandler implements RequestHandler<String, String> {
+public class ProductRequestHandler implements RequestHandler<InputDTO, String> {
 
   @Override
-  public String handleRequest(String event, Context context) {
-    context.getLogger().log(event);
-    return event + " Ha";
+  public String handleRequest(InputDTO event, Context context) {
+    context.getLogger().log("Hello, " + event.getName());
+    return event + " Hi";
   }
 }

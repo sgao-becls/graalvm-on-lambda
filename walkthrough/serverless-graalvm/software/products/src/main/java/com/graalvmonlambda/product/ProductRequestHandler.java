145b844b370bbbb9029b83133f7ff100b95fc3e9
sgao <sgao@beckman.com>
2023-04-18 21:39:32 +0800

    update

--- walkthrough/serverless-graalvm/software/products/src/main/java/com/graalvmonlambda/product/ProductRequestHandler.java
+++ walkthrough/serverless-graalvm/software/products/src/main/java/com/graalvmonlambda/product/ProductRequestHandler.java
@@ -1,13 +1,36 @@
 package com.graalvmonlambda.product;
 
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import org.apache.commons.io.IOUtils;
 
-public class ProductRequestHandler implements RequestHandler<InputDTO, String> {
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+
+public class ProductRequestHandler implements RequestStreamHandler {
 
   @Override
-  public String handleRequest(InputDTO event, Context context) {
-    context.getLogger().log("Hello, " + event.getName());
-    return event + " Hi";
+  public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) {
+    String requestJson;
+    try {
+      requestJson = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+    System.out.println(requestJson);
+    String responseJson = "Hello World!!";
+    try (PrintWriter writer =
+             new PrintWriter(
+                 new BufferedWriter(new OutputStreamWriter(outputStream, StandardCharsets.UTF_8)))) {
+      writer.write(responseJson);
+      if (writer.checkError()) {
+        throw new RuntimeException("Writer encountered an error");
+      }
+    }
   }
 }
